2acb74f84c2353bf96530b0a4a27a4eca2794967
OSpoon <zxin088@gmail.com>
2026-05-18 10:19:46 +0800

    feat: add coverage support for vitest with v8 provider and update dependencies

--- app/cli/vitest.config.ts
+++ app/cli/vitest.config.ts
@@ -8,5 +8,16 @@ export default defineConfig({
       '~': path.resolve('.'),
     },
   },
-  test: {},
+  test: {
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.ts'],
+      exclude: [
+        'src/**/index.ts',
+        'src/**/*.test-utils.ts',
+        'src/core/schema-sqlite/migrate-helper.ts',
+      ],
+      reporter: ['text', 'lcov', 'html'],
+    },
+  },
 })
